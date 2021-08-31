e6a032ee5349abf83380a89c92fcab0a3fc31522
panggggg <p.picczy@gmail.com>
2021-08-31 12:50:43 +0700

    Update home page and add social

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -4,8 +4,18 @@ export const HomePage = ({ className }) => {
     return (
         <div>
         <div className="h-screen p-20 grid grid-cols-2 grid-flow-row justify-items-center">
-            {/* <img className="mx-10" src="https://i.pinimg.com/originals/df/8e/1f/df8e1f9ea838b37140515ddd9208cac1.gif" width="570px" /> */}
-            <h1 className={className}>Welcome<br/>to<br/>my portfolio</h1>
+            <div className="span-cols-1">
+                <div className="span-rows-1">
+                    <h1 className={className}>Welcome<br/>to<br/>my portfolio</h1>
+                </div>
+                <div className="span-rows-2 pt-5">
+                    <div className="flex space-x-0 grid grid-cols-3 justify-items-center">
+                        <a href="https://github.com/panggggg" className="span-cols-1 justify-self-end ml-20"><img src="https://image.flaticon.com/icons/png/512/733/733609.png" width="30px"/></a>
+                        <a href="https://www.facebook.com/popang.pawornwan/" className="span-cols-2 justify-self-center mr-40 "><img src="https://image.flaticon.com/icons/png/512/1384/1384005.png" width="30px"/></a>
+                        <a href="https://www.instagram.com/_panggggs/" className="span-cols-3 justify-self-start"><img src="https://image.flaticon.com/icons/png/512/1384/1384015.png" width="30px"/></a>
+                    </div>
+                </div>
+            </div>
             <div className="p-20">
             <img className="" src="https://2.bp.blogspot.com/-RwKl7rSsJ7o/WD6hruu-KkI/AAAAAAALo9o/JdUdt-aBuI4SFxOFUK2rhsdPuX1yR9TwwCLcB/s1600/AS002044_03.gif" width="1000px"/>
         </div>
