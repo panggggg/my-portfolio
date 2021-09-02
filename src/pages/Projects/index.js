58d4fecc59f797c0a59c6f56c67a69d09f517f84
panggggg <p.picczy@gmail.com>
2021-09-02 16:50:39 +0700

    Update projects page

--- src/pages/Projects/index.js
+++ src/pages/Projects/index.js
@@ -1,9 +1,126 @@
 import { React } from 'react';
 
-export const ProjectPage = () => {
+export const ProjectPage = ({ className, fontProject }) => {
     return (
         <div className="h-screen">
-            <h1>ProjectPage</h1>
-        </div>
+            <div className="pt-5 pb-5">
+                <h1 className={className}>My Project</h1>
+            </div>
+            
+            <div className={fontProject}>
+            <div className="px-96 justify-center">
+
+            <div class="flex flex-col">
+            <div class="flex bg-red-200 max-w-2xl ml-40 mr-40 mb-2 shadow-lg">
+                <div class="w-20 bg-red-400">
+                    <div class="p-4">
+                        <img src="https://img-premium.flaticon.com/png/512/5305/premium/5305370.png?token=exp=1630392979~hmac=e9ba578d90bfea4c34fa7c326f0c31e0" width="50px"/>
+                    </div>
+                </div>
+                <div class="w-auto text-grey-darker text-left p-4">
+                    <span class="text-lg font-bold pb-4 text-left">
+                        Food Ordered Program
+                    </span>
+                    <p class="leading-tight">
+                        By JAVA language
+                    </p>
+                </div>
+            </div>
+            </div>
+
+            <div class="flex flex-col">
+            <div class="flex bg-pink-200 max-w-2xl ml-40 mr-40 mb-2 shadow-lg">
+                <div class="w-20 bg-pink-400">
+                    <div class="p-4">
+                        <img src="https://image.flaticon.com/icons/png/512/5231/5231909.png" width="50px"/>
+                    </div>
+                </div>
+                <div class="w-auto text-grey-darker text-left p-4">
+                    <span class="text-lg font-bold pb-4 text-left">
+                        Making question and answer game
+                    </span>
+                    <p class="leading-tight">
+                        By Visual Basic for Application
+                    </p>
+                </div>
+            </div>
+            </div>
+
+            <div class="flex flex-col">
+            <div class="flex bg-yellow-200 max-w-2xl ml-40  mr-40 mb-2 shadow-lg">
+                <div class="w-20 bg-yellow-400">
+                    <div class="p-4">
+                        <img src="https://image.flaticon.com/icons/png/512/5348/5348061.png" width="50px"/>
+                    </div>
+                </div>
+                <div class="w-auto text-grey-darker text-left p-4">
+                    <span class="text-lg font-bold pb-4 text-left">
+                        Boba Shop website for business
+                    </span>
+                    <p class="leading-tight">
+                        using PHP and SQL Server
+                    </p>
+                </div>
+            </div>
+            </div>
+
+            <div class="flex flex-col">
+            <div class="flex bg-green-200 max-w-2xl ml-40  mr-40 mb-2 shadow-lg">
+                <div class="w-20 bg-green-400">
+                    <div class="p-4">
+                        <img src="https://image.flaticon.com/icons/png/512/5099/5099105.png" width="50px"/>
+                    </div>
+                </div>
+                <div class="w-auto text-grey-darker text-left p-4">
+                    <span class="text-lg font-bold pb-4 text-left">
+                        Hotel reserve website
+                    </span>
+                    <p class="leading-tight">
+                        using PHP and SQL Server
+                    </p>
+                </div>
+            </div>
+            </div>
+
+            <div class="flex flex-col">
+            <div class="flex bg-blue-200 max-w-2xl ml-40  mr-40 mb-2 shadow-lg">
+                <div class="w-20 bg-blue-400">
+                    <div class="p-4">
+                        <img src="https://image.flaticon.com/icons/png/512/5396/5396706.png" width="50px"/>
+                    </div>
+                </div>
+                <div class="w-auto text-grey-darker text-left p-4">
+                    <span class="text-lg font-bold pb-4 text-left">
+                        Auction website
+                    </span>
+                    <p class="leading-tight">
+                        using PHP and SQL Server
+                    </p>
+                </div>
+            </div>
+            </div>
+
+            <div class="flex flex-col">
+            <div class="flex bg-purple-200 max-w-2xl ml-40  mr-40 mb-2 shadow-lg">
+                <div class="w-40 bg-purple-400">
+                    <div class="p-4">
+                        <img src="https://image.flaticon.com/icons/png/512/5099/5099382.png" width="50px"/>
+                    </div>
+                </div>
+                <div class="w-auto text-grey-darker text-left p-4">
+                    <span class="text-lg font-bold pb-4 text-left">
+                        The Association Rules for Purchasing product in convenience stores
+                    </span>
+                    <p class="leading-tight">
+                        By Weka
+                    </p>
+                </div>
+            </div>
+            </div>
+
+            </div>
+            </div>
+            
+    </div>
     )
 }
