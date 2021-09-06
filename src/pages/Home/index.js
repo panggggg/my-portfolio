17222f2df68a9026863905573dc35d26c69a7274
panggggg <p.picczy@gmail.com>
2021-09-06 16:17:28 +0700

    Update home page

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -2,7 +2,7 @@ import { React } from 'react';
 
 export const HomePage = ({ className }) => {
     return (
-        <div>
+        <div className="" >
         <div className="h-screen p-20 grid grid-cols-2 grid-flow-row justify-items-center">
             <div className="span-cols-1">
                 <div className="span-rows-1">
@@ -10,9 +10,9 @@ export const HomePage = ({ className }) => {
                 </div>
                 <div className="span-rows-2 pt-5">
                     <div className="flex space-x-0 grid grid-cols-3 justify-items-center">
-                        <a href="https://github.com/panggggg" className="span-cols-1 justify-self-end ml-20"><img src="https://image.flaticon.com/icons/png/512/733/733609.png" width="30px"/></a>
-                        <a href="https://www.facebook.com/popang.pawornwan/" className="span-cols-2 justify-self-center mr-40 "><img src="https://image.flaticon.com/icons/png/512/1384/1384005.png" width="30px"/></a>
-                        <a href="https://www.instagram.com/_panggggs/" className="span-cols-3 justify-self-start"><img src="https://image.flaticon.com/icons/png/512/1384/1384015.png" width="30px"/></a>
+                        <a href="https://github.com/panggggg" className="span-cols-1 justify-self-end ml-20 rounded-t-lg md:w-1/3 md:rounded-l-lg md:rounded-t-none"><img src="https://image.flaticon.com/icons/png/512/733/733609.png" width="30px"/></a>
+                        <a href="https://www.facebook.com/popang.pawornwan/" className="span-cols-2 justify-self-center mr-40 rounded-t-lg md:w-1/3 md:rounded-l-lg md:rounded-t-none"><img src="https://image.flaticon.com/icons/png/512/1384/1384005.png" width="30px"/></a>
+                        <a href="https://www.instagram.com/_panggggs/" className="span-cols-3 justify-self-start rounded-t-lg md:w-1/3 md:rounded-l-lg md:rounded-t-none"><img src="https://image.flaticon.com/icons/png/512/1384/1384015.png" width="30px"/></a>
                     </div>
                 </div>
             </div>
